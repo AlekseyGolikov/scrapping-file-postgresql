991195562e747441d7c6ecb39c23442872d1df52
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2022-01-11 10:00:12 +0300

    write-to-csv done

--- parse.py
+++ parse.py
@@ -5,7 +5,7 @@ def get_items_list(items):
     
     for item in items:
 
-        title = item.find('span', class_='title_list').text
+        title = item.find('span', class_='title_list').text.strip()
         author = item.find('span', class_='author_list').text.replace('. ','.')
         year = item.find('span', class_='book_year').text
 
@@ -18,7 +18,10 @@ def get_links_list(links):
         yield [link.get('href')]
 
 
-def InsertBooksList():
+def ParseBooksList():
+
+    w_file = open('result.csv', mode='w', encoding='utf-8')
+    file_writer = csv.writer(w_file, delimiter=',', lineterminator='\r')
 
     with open('text') as file:
 
@@ -29,24 +32,30 @@ def InsertBooksList():
 
         item = get_items_list(items)
         link = get_links_list(links)
+        
+        i = 1
 
         try:
             while item and link:
+                
                 row = []
                 row.append(next(item))
                 row.append(next(link))
-                break
+
+                if item and link:
+                    file_writer.writerow([i, row[0][0], row[0][1], row[0][2], row[1][0]])
+
+                i += 1
+                
         except Exception as ex:
             pass
         finally:
             # print(f'{row[0][0]} | {row[0][1]} | {row[0][2]} | {row[1][0]}')
             # print(f'{type(row[0][0])} | {type(row[0][1])} | {type(row[0][2])} | {type(row[1][0])}')
-            with open('result.csv', mode='w', encoding='utf-8') as w_file:
-                file_writer = csv.writer(w_file, delimiter=',', lineterminator='\r')
-                file_writer.writerow([1, row[0][0], row[0][1], row[0][2], row[1][0]])
+            w_file.close()
 
 
 
 
 if __name__ == "__main__":
-    InsertBooksList()
+    ParseBooksList()
--- scrap.py
+++ scrap.py
@@ -3,11 +3,16 @@ import bs4
 
 url = "http://militera.lib.ru/1/cats/all/sch/index.html"
 
-response = requests.get(url)
+def scrapWeb():
+    
+    response = requests.get(url)
 
-soup = str(bs4.BeautifulSoup(response.text, "lxml"))
+    soup = str(bs4.BeautifulSoup(response.text, "lxml"))
 
-# soup = soup.encode()
+    # soup = soup.encode()
 
-with open("text","w") as file:
-    file.write(soup)
+    with open("text","w") as file:
+        file.write(soup)
+
+if __name__=='__main__':
+    scrapWeb()
